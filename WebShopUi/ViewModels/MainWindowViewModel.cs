3eba5e5d8e1dc1b93346f95a91b3f1b7c8e48a24
Frafn24 <frafn24@student.sdu.dk>
2025-03-06 09:22:16 +0100

    First !!!

---
+++ WebShopUi/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace WebShopUi.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ WebShopUi/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WebShopUi.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
